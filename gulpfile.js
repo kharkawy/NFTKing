33748b022073fa4de25be8f7ee7438c304898181
Karolina Harkawy <kharkawy@fastmail.com>
2023-02-21 11:17:30 +0100

    divided main scss file into multiple files for better overview

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('styles', function () {
-  return gulp.src('./css/styles.scss')
+  return gulp.src('./css/main.scss')
      .pipe(sass.sync().on('error', sass.logError))
-     .pipe(gulp.dest('./css'));
+     .pipe(gulp.dest('./css/dist'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./css/styles.scss', gulp.series('styles'));
+  gulp.watch('./css/main.scss', gulp.series('styles'));
 });
 
 // Default Task
