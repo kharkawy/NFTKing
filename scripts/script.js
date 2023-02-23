402e4958c966f94ac53896ee53e406173af4d6d3
Karolina Harkawy <kharkawy@fastmail.com>
2023-02-23 15:35:20 +0100

    code cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,151 @@
-let header = document.getElementById("Header");
-let headerPos = header.getBoundingClientRect().top;
+function updateCopyrightYear() {
+  const copyrightYear = document.getElementById("CopyrightYear");
+  const currentYear = new Date().getFullYear();
 
-window.addEventListener("scroll", (e) => {
+  copyrightYear.innerHTML = currentYear;
+}
+
+function checkHeaderScroll() {
+  const header = document.getElementById("Header");
+  const headerPos = header.getBoundingClientRect().top;
+
+  window.addEventListener("scroll", (e) => {
     let scrollPos = window.scrollY;
 
     if (scrollPos > headerPos) {
-        header.classList.add("sticky");
+      header.classList.add("sticky");
     } else {
-        header.classList.remove("sticky");
+      header.classList.remove("sticky");
     }
+  });
+}
+
+function toggleMobileMenu() {
+  const burgerButton = document.getElementById("BurgerButton");
+
+  burgerButton.addEventListener("click", (e) =>
+    document.body.classList.toggle("mobile-nav-visible")
+  );
+}
+
+function toggleTabs() {
+  const toplistTabButtons = document.querySelectorAll(
+    ".toplist__tab-group .btn"
+  );
+
+  [...toplistTabButtons].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (!btn.classList.contains("active")) {
+        let activeButton = document.querySelector(
+          ".toplist__tab-group .btn.active"
+        );
+        activeButton.classList.remove("active");
+        btn.classList.add("active");
+
+        let targetTabId = btn.getAttribute("href");
+        let targetTab = document.getElementById(targetTabId);
+        let activeTab = document.querySelector(".toplist__slider.active");
+        activeTab.classList.remove("active");
+        targetTab.classList.add("active");
+
+        fadeIn(targetTab, 600);
+        fadeOut(activeTab, 600);
+      }
+    });
+  });
+
+  function fadeOut(el, speed) {
+    el.style.opacity = 1;
+
+    const start = Date.now();
+
+    function fade() {
+      const elapsed = Date.now() - start;
+      el.style.opacity = 1 - elapsed / speed;
+
+      if (elapsed < speed) {
+        requestAnimationFrame(fade);
+      } else {
+        el.style.opacity = 0;
+      }
+    }
+
+    requestAnimationFrame(fade);
+  }
+
+  function fadeIn(el, speed) {
+    el.style.opacity = 0;
+
+    const start = Date.now();
+
+    function fade() {
+      const elapsed = Date.now() - start;
+      el.style.opacity = elapsed / speed;
+
+      if (elapsed < speed) {
+        requestAnimationFrame(fade);
+      }
+    }
+
+    requestAnimationFrame(fade);
+  }
+}
+
+function toggleFollowButton() {
+  let followButtons = document.querySelectorAll(".slide__button .btn");
+
+  [...followButtons].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      btn.classList.toggle("btn-unchecked");
+      if (!btn.classList.contains("btn-unchecked")) {
+        btn.innerHTML = "Followed";
+      } else {
+        btn.innerHTML = "Follow";
+      }
+    });
+  });
+}
+
+function mountSliders() {
+  new Glide(".glide-1", {
+    perView: 4,
+    bound: true,
+    breakpoints: {
+      1250: {
+        perView: 3,
+      },
+      900: {
+        perView: 2,
+      },
+      650: {
+        perView: 1,
+      },
+    },
+  }).mount();
+  new Glide(".glide-2", {
+    perView: 4,
+    bound: true,
+    breakpoints: {
+      1250: {
+        perView: 3,
+      },
+      900: {
+        perView: 2,
+      },
+      650: {
+        perView: 1,
+      },
+    },
+  }).mount();
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  checkHeaderScroll();
+  toggleMobileMenu();
+  toggleTabs();
+  mountSliders();
+  toggleFollowButton();
+  updateCopyrightYear();
 });
-
-let burgerButton = document.getElementById("BurgerButton");
-
-burgerButton.addEventListener("click", (e) => document.body.classList.toggle("mobile-nav-visible"))
-
-let copyrightYear = document.getElementById("CopyrightYear");
-let currentYear = new Date().getFullYear();
-
-copyrightYear.innerHTML = currentYear;
