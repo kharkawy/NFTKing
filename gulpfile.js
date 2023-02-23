181dc75fd0859a1bc0c3da26a530a3f61eddff93
Karolina Harkawy <kharkawy@fastmail.com>
2023-02-23 16:00:59 +0100

    added CSS and JS minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function () {
   return gulp.src('./css/main.scss')
@@ -9,9 +12,29 @@ gulp.task('styles', function () {
      .pipe(gulp.dest('./css/dist'));
 });
 
+gulp.task('autoprefixer', function () {
+  return gulp.src('./css/dist/main.css')
+     .pipe(autoprefixer({
+       cascade: false
+     }))
+     .pipe(gulp.dest('./css/dist'))
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('./css/dist/main.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./css/dist'));
+});
+
+gulp.task('compress-js', function () {
+  return gulp.src('./scripts/script.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./scripts/dist'));
+});
+
 gulp.task('watch', function () {
-  gulp.watch('./css/**/*.scss', gulp.series('styles'));
+  gulp.watch(['./css/**/*.scss', './scripts/**/*.js'], gulp.series('styles', 'autoprefixer', 'minify-css', 'compress-js'));
 });
 
 // Default Task
-gulp.task('default', gulp.parallel('watch'));
+gulp.task('default', gulp.series('watch'));
