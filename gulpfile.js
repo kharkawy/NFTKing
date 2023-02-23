005447a2aad425d9bd757cc4f665c7e1c1f148a4
Karolina Harkawy <kharkawy@fastmail.com>
2023-02-23 17:47:15 +0100

    updated gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,60 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const del = require("del");
 
-gulp.task('styles', function () {
-  return gulp.src('./css/main.scss')
-     .pipe(sass.sync().on('error', sass.logError))
-     .pipe(gulp.dest('./css/dist'));
+gulp.task("copy", function () {
+  return gulp.src("src/index.html").pipe(gulp.dest("dist"));
 });
 
-gulp.task('autoprefixer', function () {
-  return gulp.src('./css/dist/main.css')
-     .pipe(autoprefixer({
-       cascade: false
-     }))
-     .pipe(gulp.dest('./css/dist'))
+gulp.task("glide", function () {
+  return gulp
+    .src([
+      "node_modules/@glidejs/glide/dist/glide.min.js",
+      "node_modules/@glidejs/glide/dist/css/glide.core.min.css",
+    ])
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('minify-css', function () {
-  return gulp.src('./css/dist/main.css')
+gulp.task("assets", function () {
+  return gulp.src("src/assets/**/*").pipe(gulp.dest("dist/"));
+});
+
+gulp.task("clean", function () {
+  return del(["dist/**/*"]);
+});
+
+gulp.task("css", function () {
+  return gulp
+    .src("src/scss/main.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./css/dist'));
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('compress-js', function () {
-  return gulp.src('./scripts/script.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./scripts/dist'));
+gulp.task("js", function () {
+  return gulp.src("src/js/script.js").pipe(uglify()).pipe(gulp.dest("dist"));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./css/**/*.scss', './scripts/**/*.js'], gulp.series('styles', 'autoprefixer', 'minify-css', 'compress-js'));
+gulp.task('main', function() {
+  return gulp.series("clean", "copy", "glide", "css", "js", "assets")
+})
+
+gulp.task("watch", function () {
+  gulp.watch(
+    ["src/scss/**/*.scss", "src/js/**/*.js", "src/index.html"],
+    gulp.series("clean", gulp.parallel("copy", "glide", "css", "js", "assets"))
+  );
 });
 
 // Default Task
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("clean", gulp.parallel("copy", "glide", "css", "js", "assets")));
